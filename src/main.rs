ff6b36261e5398d48ce1914cd0d14a25c546be72
Kharista <candra.kharista@gmail.com>
2022-11-28 14:50:57 +0700

    initial poc

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,28 @@
+mod convergence_duck;
 
 use std::env;
-mod convergence;
+use std::sync::Arc;
+use convergence::server::{self, BindOptions};
+use convergence_duck::duckdb::DuckDBEngine;
 use duckdb::{params, Connection, Result};
 use duckdb::arrow::record_batch::RecordBatch;
 use duckdb::arrow::util::pretty::print_batches;
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+
+async fn new_engine(db_path: String) -> DuckDBEngine {
+    let conn = Connection::open(db_path).unwrap();
+    DuckDBEngine::new(conn)
 }
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let mut db_path = String::from("./db.duckdb");
     if args.len() > 1 {
         db_path = String::from(&args[1]);
     }
 
-    let conn = Connection::open(db_path)?;
-    
-    conn.execute_batch(
-        r"CREATE SEQUENCE seq;
-          CREATE TABLE person (
-                  id              INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq'),
-                  name            TEXT NOT NULL,
-                  data            BLOB
-                  );
-        ")?;
-
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?, ?)",
-        params![me.name, me.data],
-    )?;
-
-    // query table by rows
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-
-    // query table by arrow
-    let rbs: Vec<RecordBatch> = stmt.query_arrow([])?.collect();
-    print_batches(&rbs).unwrap();
-    Ok(())
+	let port = server::run(BindOptions::new().with_port(5433), Arc::new(move || Box::pin(new_engine(db_path.clone()))))
+		.await
+		.unwrap();
 }
